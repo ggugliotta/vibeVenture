49c583f68a9f164da927226b8e51eeac1ae5d092
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-10-16 21:20:33 -0500

    updated setupTests.js mesages_to_ignore

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,18 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+//src/setupTests.js
+
+//Import Jest Assertion Library, React Testing Library already installed for components
 import '@testing-library/jest-dom';
+
+//Warning Messages Intentionally Prevented from Appearing
+const MESSAGES_TO_IGNORE = [
+    "When testing, code that causes React state updates should be wrapped into act(...):",
+    "Error:",
+    "The above error occurred"
+];
+
+const originalError = console.error.bind(console.error);
+
+console.error = (...args) => {
+    const ignoreMessage =MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
+    if (!ignoreMessage) originalError(...args);
+}
