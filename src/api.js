dc040b82c94bad6126773767f372a2f958f150d4
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-10-26 13:07:32 -0500

    changes to api.js

--- src/api.js
+++ src/api.js
@@ -3,20 +3,45 @@
 import mockData from './mock-data';
 
 /**
- * 
+ *
  * @param {*} events:
- * This function takes an events array, then uses map to create a new array with only locations. 
- * It also removes duplicates by creating another new array using the spread operator and spreading a Set.
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
 export const extractLocations = (events) => {
-    const extractedLocations = events.map((event) => event.location);
-    const locations = [...new Set(extractedLocations)];
-    return locations;
+  const extractedLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractedLocations)];
+  return locations;
 };
 
+/**
+ *
+ * This function will fetch the list of all events
+ */
+export const getEvents = async () => {
+
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = "YOUR_GET_EVENTS_API_ENDPOINT" + "/" + token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      return result.events;
+    } else return null;
+  }
+};
+
+
 export const getAccessToken = async () => {
- const accessToken = localstorage.getItem('access_token');
+  const accessToken = localStorage.getItem('access_token');
   const tokenCheck = accessToken && (await checkToken(accessToken));
 
   if (!accessToken || tokenCheck.error) {
@@ -25,7 +50,7 @@ export const getAccessToken = async () => {
     const code = await searchParams.get("code");
     if (!code) {
       const response = await fetch(
-        "https://fg3bgc5r7h.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
+        "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
       );
       const result = await response.json();
       const { authUrl } = result;
@@ -44,24 +69,6 @@ const checkToken = async (accessToken) => {
   return result;
 };
 
-export const getEvents = async () => {
-  if (window.location.href.startsWith("http://localhost")) {
-    return mockData;
-  }
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url =  "https://fg3bgc5r7h.execute-api.us-east-2.amazonaws.com/dev/api/get-events" + "/" + token;
-    const response = await fetch(url);
-    const result = await response.json();
-    if (result) {
-      return result.events;
-    } else return null; 
-  }
-};
-
 const removeQuery = () => {
   let newurl;
   if (window.history.pushState && window.location.pathname) {
@@ -77,14 +84,33 @@ const removeQuery = () => {
   }
 };
 
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    'https://fg3bgc5r7h.execute-api.us-east-2.amazonaws.com/dev/api' + '/' + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem("access_token", access_token);
+// getToken without try...catch
 
-  return access_token;
-};
-export default getEvents;
+// const getToken = async (code) => {
+//   const encodeCode = encodeURIComponent(code);
+//   const response = await fetch(
+//     'YOUR_GET_ACCESS_TOKEN_ENDPOINT' + '/' + encodeCode
+//   );
+//   const { access_token } = await response.json();
+//   access_token && localStorage.setItem("access_token", access_token);
+
+//   return access_token;
+// };
+
+// getToken with try...catch
+
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+
+    const response = await fetch('YOUR_GET_ACCESS_TOKEN_ENDPOINT' + '/' + encodeCode);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    error.json();
+  }
+}
