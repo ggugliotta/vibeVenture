feb66314098465822e13188ef6f235afa0f15212
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-11-07 11:39:37 -0600

    changed x and y axis in cityeventschart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
+import CityEventsChart from './components/CityEventsChart';
 
 import "./App.css";
 
@@ -39,6 +40,7 @@ useEffect(() => {
 
 return (
     <div className="App">
+      <h1>VibeVenture App</h1>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text ={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text ={errorAlert}/> : null}
@@ -46,6 +48,7 @@ return (
       </div>
       <CitySearch allLocations={allLocations} setCurrentCity = {setCurrentCity} setInfoAlert={setInfoAlert}/>
       <NumberOfEvents setCurrentNOE ={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events}/>
     </div>
   );
